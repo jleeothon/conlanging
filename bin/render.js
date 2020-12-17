0929f45e3a6892928a31a9515b301deafdf70c02
Johnny Lee-Othon <jleeothon@icloud.com>
2020-12-17 18:00:35 +0100

    Update script to generate rendered md

--- bin/render.js
+++ bin/render.js
@@ -1,7 +1,9 @@
 #!/usr/bin/env node
 
+const {readFileSync, writeFileSync} = require('fs');
+const {join} = require('path');
+
 const handlebars = require('handlebars');
-const {readFileSync} = require('fs');
 const yaml = require('js-yaml');
 
 const templateSource = `# Theudisk
@@ -10,31 +12,48 @@ const templateSource = `# Theudisk
 
 {{#each entries}}
 ### {{word}}
-{{#if part}}
 
-({{part}})
-
-{{/if}}
-{{#each inflections}}
-- _{{@key}}:_ **{{this}}**
-{{/each}}
+{{#if part}}({{part}}){{/if}} {{#each inflections}}{{#unless @first}}, {{/unless}} _{{@key}}:_ **{{this}}**{{/each}}
 
 {{meaning}}
 
 {{#if see}}
-
-See:
-
-{{#each see}}
-- [{{this}}](#{{this}})
-{{/each}}
+See: {{#each see}}{{#unless @first}}, {{/unless}}[{{this}}](#{{this}}){{/each}}
 {{/if}}
 
 {{/each}}
 `;
 
-const data = yaml.safeLoad(readFileSync(0, 'utf-8'));
-
 const template = handlebars.compile(templateSource);
 
-console.log(template(data));
+const outDir = './out';
+
+async function writeFile(fileName, title, data) {
+	const dataWithTitle = {title, ...data};
+	writeFileSync(join(outDir, fileName), template(dataWithTitle));
+}
+
+async function run() {
+	const data = yaml.safeLoad(readFileSync(0, 'utf-8'));
+
+	await writeFile('word-list.md', 'Word list', data);
+
+	const categoriesSet = new Set();
+	data.entries.forEach(({categories}) => categories && categories.forEach(c => categoriesSet.add(c)));
+
+	categoriesSet.forEach(category => {
+		const entries = data.entries.filter(({categories}) => categories && categories.indexOf(category) >= 0);
+		const newData = {entries: entries};
+		writeFile(category + '.md', category, newData);
+	});
+
+	const parts = ['noun', 'adjective', 'conjunction', 'preposition', 'adverb'];
+	parts.forEach(part => {
+		const entries = data.entries.filter(({part: p}) => p && p === part);
+		const newData = {entries: entries};
+		writeFile(part + '.md', part, newData);
+	});
+}
+
+run();
+
---
+++ bin/sort-word-list.fish
@@ -0,0 +1,3 @@
+#!/usr/bin/env fish
+
+bin/sort-word-list.fish < data/word-list.yaml
