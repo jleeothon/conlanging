537506721a3e13134fd0018d02e64e7fc15c74bb
Johnny Lee-Othon <jleeothon@icloud.com>
2022-09-07 22:59:05 +0200

    Update derivations and data

--- packages/derivations/transform.js
+++ packages/derivations/transform.js
@@ -286,7 +286,7 @@ function run() {
 		apply(finalEntries, transformation);
 	}
 
-	writeFileSync("./transform.json", JSON.stringify(finalEntries, null, 2));
+	writeFileSync("./data/transform.json", JSON.stringify(finalEntries, null, 2));
 }
 
 run();
