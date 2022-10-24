bdcf7629ca1a9d8f507733df52feeb55a0f8c4f1
Johnny Lee-Othon <jleeothon@icloud.com>
2022-10-24 12:12:19 +0200

    Update code style

--- bin/verify-word-list.js
+++ bin/verify-word-list.js
@@ -1,30 +1,32 @@
 #!/usr/bin/env node
 
-import {readFileSync} from 'node:fs';
-import path from 'node:path';
-import process from 'node:process';
+import { readFileSync } from "node:fs";
+import path from "node:path";
+import process from "node:process";
 
-import Ajv from 'ajv';
-import yaml from 'js-yaml';
+import Ajv from "ajv";
+import yaml from "js-yaml";
 
-import projectRoot from '../lib/project-root.js';
+import projectRoot from "../lib/project-root.js";
 
-const schemaPath = path.join(projectRoot, 'word-list-schema.json');
+const schemaPath = path.join(projectRoot, "word-list-schema.json");
 
-const schema = yaml.load(readFileSync(schemaPath, 'utf-8'));
+const schema = yaml.load(readFileSync(schemaPath, "utf8"));
 
-const ajv = new Ajv({allErrors: true, verbose: true});
+const ajv = new Ajv({ allErrors: true, verbose: true });
 
-const input = yaml.load(readFileSync(0, 'utf-8'));
+const input = yaml.load(readFileSync(0, "utf8"));
 
 const validate = ajv.compile(schema);
 const valid = validate(input);
 
 if (valid) {
-	console.error('✓ Word list OK');
+	console.error("✓ Word list OK");
 } else {
-	for (const error of validate.errors
-		.map(({instancePath, message}) => ({path: instancePath, message}))) {
+	for (const error of validate.errors.map(({ instancePath, message }) => ({
+		path: instancePath,
+		message,
+	}))) {
 		console.error(error);
 	}
 
