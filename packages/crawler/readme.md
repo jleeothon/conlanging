d5bfc78215180511a0110599dc58e1da1666b9d8
Johnny Lee-Othon <jleeothon@icloud.com>
2022-12-08 23:40:07 +0100

    Update word list and scripts

--- packages/crawler/readme.md
+++ packages/crawler/readme.md
@@ -7,3 +7,7 @@ node crawl.js
 # data-1.json
 node cleanup.js
 ```
+
+To do:
+
+- https://www.mediawiki.org/wiki/API:Get_the_contents_of_a_page
--- packages/word-list/lib/alphabet.ts
+++ packages/word-list/lib/alphabet.ts
@@ -1,9 +1,8 @@
-// import * as naturalCompare from "string-natural-compare";
+import naturalCompare from "string-natural-compare";
 
 export const alphabet =
-	"-AaÄäBbCcDdEeËëFfGgHhIiÏïJjKkLlMmNnOoÖöŐőPpQqRrSsTtÞþUuÜüŰűVvWwXxYyZz";
+	"AaÄäBbCcDdEeËëFfGgHhIiÏïJjKkLlMmNnOoÖöŐőPpQqRrSsTtÞþUuÜüŰűVvWwXxYyZz-";
 
 export function compare(a: string, b: string) {
-	// return naturalCompare(a, b, { alphabet });
-	return a.localeCompare(b);
+	return naturalCompare(a, b, { alphabet });
 }
