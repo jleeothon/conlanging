27d70b9065f35ca2878ef2346f9bfc2587929b66
Johnny Lee-Othon <jleeothon@icloud.com>
2022-12-10 12:58:13 +0100

    Update json file style

--- packages/query/index.js
+++ packages/query/index.js
@@ -1,23 +1,23 @@
-import process from 'node:process';
+import process from "node:process";
 
-import rx from 'rxjs';
-import {program} from 'commander';
+import rx from "rxjs";
+import { program } from "commander";
 
-import data from 'crawler';
+import data from "crawler";
 
 async function search(regexp) {
 	const dataObservable = rx.from(data);
 	const re = new RegExp(regexp);
 	dataObservable
 		.pipe(
-			rx.filter(({lemma}) => re.test(lemma)),
-			rx.map(({lemma}) => lemma),
+			rx.filter(({ lemma }) => re.test(lemma)),
+			rx.map(({ lemma }) => lemma)
 		)
-		.subscribe(lemma => {
+		.subscribe((lemma) => {
 			console.log(lemma);
 		});
 }
 
-program.argument('<regexp>', 'regex to search').action(search);
+program.argument("<regexp>", "regex to search").action(search);
 
 program.parse(process.argv);
