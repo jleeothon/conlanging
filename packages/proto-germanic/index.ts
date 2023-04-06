bb4720d4aeaa2e40e1df45d4ab5c6166ed7d66ee
Johnny Lee-Othon <jleeothon@icloud.com>
2023-04-06 22:45:42 +0200

    Update proto-germanic

--- packages/proto-germanic/index.ts
+++ packages/proto-germanic/index.ts
@@ -1,5 +1,12 @@
-import query from "@jleeothon/wiktionary-scraper";
+import { WiktionaryScraper } from "@jleeothon/wiktionary-scraper";
 
-await query("Category:Proto-Germanic_lemmas", {
+import { packageDirectorySync } from "pkg-dir";
+import path from "node:path";
+
+const cacheDir = path.join(packageDirectorySync()!, "cache");
+
+const scraper = new WiktionaryScraper(cacheDir, { secondsCache: 60 * 60 * 24 });
+
+await scraper.query("Category:Proto-Germanic_lemmas", {
 	filterPrefix: "Reconstruction:Proto-Germanic/",
 });
